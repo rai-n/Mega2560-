13b496848e20c3625ddd1adf3e4ba885c9a53e6d
rai-n <neeraj.rai@city.ac.uk>
2020-07-24 22:50:23 +0100

    Create Fade.ino

---
+++ Components/RGB-LED/Fade.ino
@@ -0,0 +1,35 @@
+#define RED 3 
+#define GREEN 5
+#define BLUE 6
+
+void setup(){
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(BLUE, LOW);
+}
+
+int red;
+int green;
+int blue; 
+
+void loop()
+{
+  red = 0;
+  green = 255;
+  blue = 0;
+
+  //Slowly turns green light into blue light. 
+  for (int i = 0; i < 255; i += 1){
+      green --;
+      blue ++; 
+      
+      analogWrite(BLUE, blue);
+      analogWrite(GREEN, green);
+      delay(10);
+  }
+  
+}
