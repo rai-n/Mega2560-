56acfdda48c155687bb37889b6ec960be5011cd6
rai-n <neeraj.rai@city.ac.uk>
2020-08-08 19:44:39 +0100

    Create Timer.ino
    WIP errors

---
+++ [Project] - Kitchen Timer/Timer.ino
@@ -0,0 +1,26 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Tomato timer");
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print( String(((1500 - millis() / 1000)/60)) + ":" + String(
+    
+     ( ( (60 - millis() / 1000) >= 0) ? (60 - millis() / 1000) : (60 - millis() / 1000)
+    
+    
+    
+    )));
+}
